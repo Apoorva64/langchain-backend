dbcd5baba628405973c4157c0120f81c9a97294d
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2024-05-10 16:23:55 +0200

    feat: add Chains

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,32 @@
 # Register your models here.
 from django.contrib import admin
-from .models import NodeType, EdgeType, Node, Edge
+from django.db import models
+from djangocodemirror.widgets import CodeMirrorWidget
+
+from .models import NodeType, EdgeType, Node, Edge, Chain, ChainNode
 
 admin.site.register(NodeType)
 admin.site.register(EdgeType)
-admin.site.register(Node)
 admin.site.register(Edge)
+
+
+class NodeAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.JSONField: {'widget': CodeMirrorWidget(config_name='javascript', embed_config=True)}
+
+    }
+
+
+admin.site.register(Node, NodeAdmin)
+
+
+class ChainNodeInline(admin.TabularInline):
+    model = ChainNode
+    extra = 1
+
+
+class ChainAdmin(admin.ModelAdmin):
+    inlines = [ChainNodeInline]
+
+
+admin.site.register(Chain, ChainAdmin)
--- api/models.py
+++ api/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from jsonschema import validate, exceptions
+from django.core.exceptions import ValidationError
+from langchain.llms import get_type_to_cls_dict
 
 
 # Create your models here.
@@ -24,8 +27,25 @@ class Node(models.Model):
     node_type = models.ForeignKey(NodeType, on_delete=models.CASCADE)
     data = models.JSONField()
 
+    def clean_fields(self, exclude=None):
+        super().clean_fields(exclude=exclude)
+        # validate data against json schema
+        if self.data:
+            try:
+                validate(self.data, self.node_type.json_schema)
+            except exceptions.ValidationError as e:
+                raise ValidationError(
+                    {'data': f'Invalid data: {e.message}'}
+                )
+
     def __str__(self):
-        return self.node_type
+        return "Node: " + str(self.node_type)
+
+    def create_node(self):
+        if self.node_type.category == "LLM":
+            node = get_type_to_cls_dict()[self.node_type.name]()(**self.data)
+            return node
+        raise ValueError("Node type not supported")
 
 
 class Edge(models.Model):
@@ -37,7 +57,25 @@ class Edge(models.Model):
         return f'{self.node_a} -> {self.node_b}'
 
 
+class Chain(models.Model):
+    name = models.CharField(max_length=100)
+    nodes = models.ManyToManyField(Node, through='ChainNode')
+
+    def __str__(self):
+        return self.name
+
+    def create_chain(self):
+        # use | to concatenate the nodes
+        chain = self.nodes.first().create_node()
+        for chain_node in self.nodes.all()[1:]:
+            chain |= chain_node.create_node()
+        return chain
 
 
+class ChainNode(models.Model):
+    chain = models.ForeignKey(Chain, on_delete=models.CASCADE)
+    node = models.ForeignKey(Node, on_delete=models.CASCADE)
+    order = models.IntegerField()
 
-
+    def __str__(self):
+        return f'{self.chain} - {self.node} - {self.order}'
